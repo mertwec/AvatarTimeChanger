ad34ac67bdc325e9888f6fbba675f76ff66f5dac
Walentin <seryimir@gmail.com>
2021-04-28 11:38:55 +0300

    rework in async main()

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
-import config
-from utilites import get_current_time, generate_image
-from telethon.sync import TelegramClient
+import resources.config as config
+from resources.utilites import get_current_time, generate_image
+from telethon import TelegramClient
 from telethon.tl.functions.photos import (UploadProfilePhotoRequest, 
-										DeletePhotosRequest)
+                                          DeletePhotosRequest)
+import asyncio
+import time
 
-def main():
-	previouse_time = ''
-	with TelegramClient(config.session_name, config.api_id, config.api_hash) as client:
-		while True:
-			if not previouse_time == get_current_time():
-				current_time = get_current_time()
-				previouse_time = current_time
-				generate_image(current_time)
-				
-				image = client.upload_file('time_image.jpg')
-				client(DeletePhotosRequest(client.get_profile_photos('me')))
-				client(UploadProfilePhotoRequest(image))
-	
+
+async def main():
+    previouse_time = ''
+    async with TelegramClient(config.session_name, config.api_id, config.api_hash) as client:
+        while True:
+            if not previouse_time == get_current_time():
+                current_time = get_current_time()
+                previouse_time = current_time
+                generate_image(current_time)
+                
+                image = await client.upload_file('time_image.jpg')
+                await client(DeletePhotosRequest(await client.get_profile_photos('me')))
+                await client(UploadProfilePhotoRequest(image))
+                time.sleep(10)
+                #print('update time')
+                
+
+
 if __name__ == "__main__":
-        #print('-=start time=-')
-        main()
+    #print('-=start time=-')
+    asyncio.run(main())
+    #asyncio.get_event_loop().run_until_complete(main())
---
+++ resources/utilites.py
@@ -0,0 +1,34 @@
+from PIL import Image, ImageFont, ImageDraw
+from datetime import datetime
+from pytz import timezone
+
+def get_current_time():
+    '''calculation of the current time,
+    depending on the time zone
+    '''
+    # times= datetime.now(timezone('Europe/Kiev')).strftime('%H:%M')
+    time_hm = datetime.now(timezone('Europe/Kiev'))
+    tuple_time_hm = time_hm.timetuple()
+    hour = str(hex(tuple_time_hm.tm_hour)).upper()
+    minutes = str(hex(tuple_time_hm.tm_min)).upper()
+    # return times # time in 'int'
+    return f'x{hour[2:]}:x{minutes[2:]}' # time in 'hex'
+
+def generate_image(text):
+    '''create picture 500x500 (.jpg) with a hex-time
+    '''
+    image = Image.new('RGB', (500,500), color='black')
+    h,w = image.size
+    drawning = ImageDraw.Draw(image)
+
+    font = ImageFont.truetype(font = 'resources/Chiller.ttf', size=200)
+    wt, ht = drawning.textsize(text, font=font) # size of text
+    drawning.text(((w - wt) / 2, (h - ht) / 2 ), text, font=font, fill='#1BDD1E') # прорисовка текста
+
+    image.save('time_image.jpg')
+
+
+if __name__ == "__main__":
+    curent_time = get_current_time()
+    generate_image(curent_time)
+
